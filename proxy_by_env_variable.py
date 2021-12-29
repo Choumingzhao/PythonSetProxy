573e8416942bcddf23a32196a1e555a785ecb17c
Choumingzhao <choumingzhao@outlook.com>
2021-12-29 14:49:36 +0800

    first commit

---
+++ proxy_by_env_variable.py
@@ -0,0 +1,20 @@
+# Seting the environment variable to the proxy server."""
+import os
+
+# get IP of parent Windows machine
+nameserver = os.environ['nameserver']
+if nameserver:
+    proxy_config = f"http://{nameserver}:17890"
+else:
+    raise Exception("Cannot find IP for parent Windows machine.")
+os.environ['http_proxy'] = proxy_config
+os.environ['https_proxy'] = proxy_config
+
+# hot patch code to set proxy for urllib.request
+import urllib.request as ur
+proxy_handler = ur.ProxyHandler({'http': proxy_config,
+                                 'https': proxy_config})
+opener = ur.build_opener(proxy_handler)
+ur.install_opener(opener)
+
+print(f'Proxy set to: {proxy_config}')
